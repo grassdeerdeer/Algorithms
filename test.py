34fd8f42ad2badeaab04424b0c237160228d0d7a
grassdeerdeer <2758696899@qq.com>
2020-02-07 11:22:22 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+def Insertion_sort(A):
+    for j in range(1,len(A)):
+        key=A[j]
+        i=j-1
+        while i>=0 and key<A[i]:
+            A[i+1]=A[i]
+            i=i-1
+        A[i+1]=key
+    return A;
+A=[5,2,4,6,1,3]
+print(Insertion_sort(A))
